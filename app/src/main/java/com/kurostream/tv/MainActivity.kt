bfa734e48dc90fb99a5fd0a7e4cb3e12f31e1568
Vasu Gupta <vasu.ragv2013@gmail.com>
2026-05-19 15:08:51 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/kurostream/tv/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.kurostream.tv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.kurostream.tv.navigation.NavGraph
+import com.kurostream.tv.ui.theme.KuroStreamTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KuroStreamTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavGraph()
+                }
+            }
+        }
+    }
+}
